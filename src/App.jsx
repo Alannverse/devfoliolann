ff718ae4491b157a684ee510d849ef0a66dcfe0a
Praseptialan <alanpraseptialan@gmail.com>
2025-07-25 13:26:14 +0800

    Fix: remove/match Contact.jsx import

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Home from './pages/home';
 import About from './pages/about';
 import Projects from './pages/projects';
 import Certificates from './pages/certificates'; // ✅ Fixed capitalization
-import Contact from './pages/Contact';
+import Contact from './pages/contact';
 import Footer from './components/Footer';
 
 function App() {
